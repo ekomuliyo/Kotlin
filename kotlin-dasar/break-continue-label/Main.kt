61dadb2e3eeca6a7b80106e86cad3e2eb157e6ea
eko muliyo <ekomuliyo@gmail.com>
2020-04-21 07:06:39 +0700

    break-continue-label

---
+++ kotlin-dasar/break-continue-label/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    // menggunakan label loop@ di awal untuk mendefinisikan label
+    // dan @loop adalah untuk menandai bahwa jika kondisi j > 3 maka break dan label loop@ yang didefinisikan diawal, proses loop tsb jg dihentikan 
+    loop@ for (i in 1..5){
+        println("Outside Loop")
+
+        for (j in 1..5){
+            println("Inside Loop")
+            if(j > 3) break@loop
+        }
+    }
+}
